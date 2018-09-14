9c7674c12a6f6b319175cc6ff35c5b8c805dc772
mindcrash <oliveremich@gmail.com>
2018-09-14 03:35:03 +0200

    added success check to build script on linux

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ bin/godot.x11.tools.64.mono --generate-mono-glue modules/mono/glue &&
 
 ### Build binaries normally
 # Editor
-scons platform=x11  -j6  target=release_debug tools=yes module_mono_enabled=yes &&
+scons platform=x11  -j6  target=release_debug tools=yes module_mono_enabled=yes mono_glue=yes &&
 # Export templates
-scons platform=x11 -j6  target=debug tools=no module_mono_enabled=yes &&
-scons platform=x11 -j6  target=release tools=no module_mono_enabled=yes 
+scons platform=x11 -j6  target=debug tools=no module_mono_enabled=yes mono_glue=yes&&
+scons platform=x11 -j6  target=release tools=no module_mono_enabled=yes mono_glue=yes
