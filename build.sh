2b3e3286af319d99689be3f15c06e6fef0d0408e
mindcrash <oliveremich@gmail.com>
2018-09-19 13:52:50 +0200

    changed build script to also compile for windows via mingw and wine installed mono

--- build.sh
+++ build.sh
@@ -1,12 +1,46 @@
-#! /bin/bash
-# Build temporary binary
-scons -j6  platform=x11 tools=yes module_mono_enabled=yes mono_glue=no &&
+
+build_godot(){
+
+if [ "$1" == "windows" ]; then
+ wine="wine "
+ export MONO64_PREFIX="${HOME}/.wine/drive_c/Program Files/Mono/"
+ extension=".exe"
+else
+ wine=""
+ export MONO64_PREFIX=""
+fi
+
+scons -j6  platform=$1 tools=yes module_mono_enabled=yes mono_glue=no &&
 # Generate glue sources
-bin/godot.x11.tools.64.mono --generate-mono-glue modules/mono/glue &&
+$wine bin/godot.$1.tools.64.mono$extension --generate-mono-glue modules/mono/glue &&
 
 ### Build binaries normally
 # Editor
-scons platform=x11  -j6  target=release_debug tools=yes module_mono_enabled=yes mono_glue=yes &&
+scons platform=$1  -j6  target=release_debug tools=yes module_mono_enabled=yes mono_glue=yes &&
 # Export templates
-scons platform=x11 -j6  target=debug tools=no module_mono_enabled=yes mono_glue=yes&&
-scons platform=x11 -j6  target=release tools=no module_mono_enabled=yes mono_glue=yes
+scons platform=$1 -j6  target=debug tools=no module_mono_enabled=yes mono_glue=yes&&
+scons platform=$1 -j6  target=release tools=no module_mono_enabled=yes mono_glue=yes
+}
+
+PLATFORMS=("windows" "x11" "all")
+
+if [ -z "$1" ]; then
+  PLATFORM="x11"
+else
+ PLATFORM=$1
+fi
+
+if [[ ! " ${PLATFORMS[@]} " =~ " ${PLATFORM} " ]]; then
+  echo "Platform ${PLATFORM} not found,maybe misspelled?"
+  exit
+fi
+if [  "$PLATFORM" == "all" ]; then
+ for plat in $PLATFORMS; do
+   if [ ! "$plat" == "all" ]; then
+   build_godot $plat
+   fi
+ done 
+else
+ build_godot $PLATFORM
+fi
+
